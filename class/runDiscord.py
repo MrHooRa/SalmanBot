e9a548606e0720f1f62afdad76d6fa5b2f2450e4
Salman <50276554+MrHooRa@users.noreply.github.com>
2021-07-18 16:04:52 +0300

    Update and add (bot_prefix, bot_description)

--- class/runDiscord.py
+++ class/runDiscord.py
@@ -2,30 +2,53 @@ import sys
 sys.path.append("class")
 
 # Packages
+import discord
 from discord.ext import commands
 from logs import *
 
 # This class to run discord bot
 class runDiscord():
-    def __init__(self, TOKEN, prefix = '$'):
+    def __init__(self, TOKEN, prefix = '$', description = 'SalmanBot'):
         self.logs = Logs(name='runDiscord.py')
         self.TOKEN = TOKEN
         self.prefix = prefix
+        self.description = description
+
+        intents = discord.Intents.default()
+        intents.members = True
+
         try:
-            self.client = commands.Bot(command_prefix="$")
+            self.client = commands.Bot(command_prefix="$", intents=intents, description=description)
         except Exception as e:
             self.logs.log("\"runDiscord.py\" cannot init\n{e}\n", True, type="Error")
         
-    def getClient(self):
+    def bot_client(self):
         """Get client"""
         return self.client
 
-    def prefix(self, setPrefix = "__defualt__"):
+    def bot_prefix(self, setPrefix="__defualt__", author="Bot"):
         """Get prefix or set new one"""
         if setPrefix == "__defualt__":
             return self.prefix
         else:
-            self.prefix = str(setPrefix)
+            try:
+                self.logs.log(f"Set new prefix ({self.prefix}) -> ({setPrefix})", True, type="command", author=author)
+                self.prefix = str(setPrefix)
+                self.client.command_prefix = self.prefix
+                return True
+            except Exception as e:
+                self.logs.log("Error while change bot_prefix", True, "Error", author=author)
+                return False
+
+    def bot_description(self, setDescription = "__defualt__", author="Bot"):
+        """Get description or set new one"""
+        if setDescription == "__defualt__":
+            return self.description
+        else:
+            self.logs.log(f"Set new description ({self.description}) -> ({setDescription})", True, type="command", author=author)
+            self.description = str(setDescription)
+            self.client.description = self.description
+            return self.description
 
     def run(self):
         """Run SalmanBot"""
