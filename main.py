91f550e4bb9e592ce1166103cfc5393e98926c3d
Salman <50276554+MrHooRa@users.noreply.github.com>
2021-07-23 08:38:34 +0300

    Move test1 (prefix) command to commands.py and move tempChannel to tempChannel.py

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ This discord bot can do:-
 
 """
 
+#TODO: Do NOT forget to put Forbidden exception in all classes!
+# Forbidden: For bot, if the bot did not have permission to do something.
+
 BOT_DETAILS = {
     'name':         "SalmanBot",
     'author':       "Salman, MrHora",
@@ -38,6 +41,7 @@ BOT_DETAILS = {
     'temp_channel_id': 867471772441903135,
     'temp_channel_category': 866660422282379274,
     'temp_channel_name': "$MEMBER_NAME$ Channel",         # Use $MEMBER_NAME$ for member name
+    'admin_roles': []
 }
 
 # DO NOT EDIT THIS!
@@ -49,7 +53,7 @@ BOT_ATT = {
 import sys
 
 # Defualt packages
-import os, asyncio, random
+import asyncio
 import logging
 
 # SalmnBot classes
@@ -58,14 +62,13 @@ import runDiscord
 from logs import *
 from commands import Commands
 from tasks import Tasks
+from tempChannel import TempChannels
 
 # Get env values
 from decouple import config
 
 # Discord packages
 import discord
-from discord import VoiceChannel
-from discord.ext import tasks
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -82,7 +85,6 @@ dataBase = (config('DB_HOSTNAME'),
             config('DB_NAME'))
 # ^=====================================================^ #
 
-
 # V=================== Discord details ==================V #
 rd = runDiscord.runDiscord(config('TOKEN'), description=f"[!] {BOT_DETAILS['name']} {BOT_DETAILS['verison']}", prefix=BOT_DETAILS['default_prefix'])
 client = rd._client()
@@ -92,99 +94,36 @@ client.command_prefix = BOT_DETAILS['default_prefix']
 # Bot on_ready
 @client.event
 async def on_ready():
-    tempChannel.start()
+    # tempChannel.start()
     logs.log(f"Logged in as (Name: {client.user.name}, ID: {client.user.id})", True)
 
     # Set guild
     BOT_ATT['guild'] = client.get_guild(BOT_DETAILS['guild_id'])
 
+    # Set temp channle
+    BOT_ATT['temp_channel'] = client.get_channel(BOT_DETAILS['temp_channel_id'])
+
     # Set temp category
     categories = BOT_ATT['guild'].categories
 
-    # Searh on category
+    # Searh on category and get temp category
     for cg in categories:
         if cg.id == BOT_DETAILS['temp_channel_category']:
             BOT_ATT['temp_channel_category'] = cg
             break
 
-#00000000000000000000000Test Area0000000000000000000000000
-@client.command(pass_context=True, name="test1", hidden=True)
-async def _test(ctx, a):
-    if rd._prefix(a, author=ctx.author.name):
-        await ctx.reply('تم التغيير بنجاح!', mention_author=True)
-    else:
-        await ctx.reply("Error!", mention_author=True)
-#000000000000000000000000000000000000000000000000000000000
-
-# TEMP CHANNELS
-
-#TODO: Take this function to tasks.py or tempChannel.py
+#TODO: Take this function to tasks.py or create tempChannel.py #DONE (To tmepChannel.py)
 #TODO: Store all temp channel to corrent            #DONE
-#TODO: Make new command for temp channel memnber to kick another member from it own channel (Something like: $dc @member)
-
-corrent_tempChannels = []
-# corrent_tempChannel:
-#   [..], <- Temp channel array
-#   [
-#    channel_name,
-#    category,
-#    member
-#   ]
-
-@tasks.loop(seconds=1)
-async def tempChannel():
-    """Make temp channel and move member to it. When all members leave the channel, it'l deleted"""
-    try:
-        for channel in corrent_tempChannels:
-            memChannel = []
-            for member in channel[0].members:
-                memChannel.append(member)
-            if len(memChannel) == 0 :
-                logs.log(f"Channel removed (channel name: {channel[0].name}, Category: {channel[2].name}, Member ID: {channel[2].id})", True)
-                await channel[0].delete()
-                corrent_tempChannels.remove(channel)
-
-    # If channel not found (Most likely member deleted its own channel)
-    except discord.NotFound as e_NF:
-        corrent_tempChannels.remove(channel)
-
-    except Exception as e:
-        logs.log(f"Can not delete channel. Exception -> {e}", True, type="Error")
-
-    try:
-        # Get Temp channel ID
-        tempChannel = client.get_channel(BOT_DETAILS['temp_channel_id'])
-        members = tempChannel.members
-        
-        # For all members in temp channel
-        for member in members:
-            tempChannelName = BOT_DETAILS['temp_channel_name'].replace("$MEMBER_NAME$", member.name)
-
-            # Create new temp channel and move user to it!
-            createdChannel = await BOT_ATT['guild'].create_voice_channel(tempChannelName, category=BOT_ATT['temp_channel_category'])
-            await member.move_to(createdChannel)
-
-            # Set permission (manage_channels) to user
-            await createdChannel.set_permissions(member, manage_channels=True, move_members=True)
-
-            # Insert new temp channel details to corrent_tempChannels array
-            corrent_tempChannels.append([createdChannel, BOT_ATT['temp_channel_category'], member])
-
-            # For log
-            logs.log(f"Create new channel (Channel name: {tempChannelName}, Catrgory: {BOT_ATT['temp_channel_category']}, Member ID: {member.id})", True)
-
-    except Exception as e:
-        logs.log(f"Can not create new temp channel. Exception -> {e}", True, type='Error')
-
+#TODO: Try pass att to tempChannel by creating new function in tempChannel.py
 
 # Cogs
 try:
-    rd.add_cog(Commands(client))
+    rd.add_cog(Commands(client, rd))
     rd.add_cog(Tasks(client))
+    rd.add_cog(TempChannels(client, BOT_DETAILS))
 except Exception as e:
     logs.log(f"{BOT_DETAILS['name']} NOT READY. -> Exception: {e}", True, type="Error")
     
-
 msg = f"""
     ***********************************
             {BOT_DETAILS['name']} {BOT_DETAILS['verison']}
